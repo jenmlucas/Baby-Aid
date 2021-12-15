9bdd8ef7abc7e1a12f83468ce53e7ebd942b6b99
B-Litty <bradlitman94@gmail.com>
2021-12-14 20:55:25 -0700

    Models and associations complete

---
+++ models/Parent.js
@@ -0,0 +1,61 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt')
+const sequelize = require('../config/connection');
+
+// create our Parent model
+class Parent extends Model {
+    // set up a method to run on instance data (per Parent) to check password
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password)
+    }
+};
+
+// create fields/columns for Parent model
+Parent.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [4]
+            }
+        }
+    },
+    {
+        hooks: {
+            async beforeCreate(newParentData) {
+                newParentData.password = await bcrypt.hash(newParentData.password, 10);
+                return newParentData;
+            },
+            async beforeUpdate(updatedParentData) {
+                updatedParentData.password = await bcrypt.hash(updatedParentData.password, 10);
+                return updatedParentData;
+            }
+        },
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'parent'
+    }
+);
+
+module.exports = Parent;
---
+++ models/Question.js
@@ -0,0 +1,65 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+
+class Question extends Model {
+    static vote(body, models) {
+        return models.Vote.create({
+            parent_id: body.parent_id,
+            question_id: body.question_id
+        }).then(() => {
+            return Post.findOne({
+                where: {
+                    id: body.question_id
+                },
+                attributes: [
+                    'id',
+                    'content',
+                    'title',
+                    'created_at',
+                    [
+                        sequelize.literal('(SELECT COUNT(*) FROM vote WHERE question.id = vote.question_id)'),
+                        'vote_count'
+                    ]
+                ]
+            });
+        });
+    }
+}
+
+Question.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        content: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [4]
+            }
+        },
+        parent_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'parent',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'question'
+    }
+);
+
+module.exports = Question;
