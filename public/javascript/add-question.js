1f08b270cd742bd1d00d14da29f1b6935cc365cc
jenmlucas <jenmlucas1@gmail.com>
2021-12-16 21:13:22 -0700

    added js files content

---
+++ public/javascript/add-question.js
@@ -0,0 +1,25 @@
+async function newQuestionFormHandler (event) {
+    event.preventDefault();
+
+    const title = document.querySelector('input[name="question-title"]').value;
+    const content = document.querySelector('input[name="question-content"]').value;
+
+    const response = await fetch('/api/questions', {
+        method: 'POST',
+        body: JSON.stringify({
+            title,
+            content
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    if (response.ok) {
+        document.location.replace('/dashboard')
+    } else {
+        alert(response.statusText)
+    }
+}
+
+document.querySelector('#new-question-form').addEventListener('submit', newQuestionFormHandler);
